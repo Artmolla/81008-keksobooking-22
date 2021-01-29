44fb6b1a6ab7ec01ef80a4e789aca368a6d3cfe3
Artem <artmolla@gmail.com>
2021-01-29 23:46:56 +0200

    Переписал функции

--- js/main.js
+++ js/main.js
@@ -1,7 +1,15 @@
-const generateRandomNumber = function (min, max, floatPrecision) {
+const generateRandomInteger = function (min, max) {
+  if ( min >= 0 && max >= 0 ) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } return false;
+}
+
+const generateRandomFloat = function (min, max, floatPrecision) {
   if ( min >= 0 && max >= 0 ) {
     return (Math.random() * (max - min + 1) + min).toFixed(floatPrecision);
-  }
+  } return false;
 }
 
 
@@ -9,5 +17,6 @@ const validateStrLength = (str, max) => {
   return str.length <= max ? true : false;
 }
 
-generateRandomNumber(10, 100);
+generateRandomInteger(10, 100);
+generateRandomFloat(10, 100, 2);
 validateStrLength('Should return false, because it\'s too long', 10);
