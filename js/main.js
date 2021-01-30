074bbf6b24aff5d75c3a5c9331b2cb1a5e5b5244
Artem <artmolla@gmail.com>
2021-01-30 23:00:42 +0200

    Исправил функцию генерации случайного целого числа

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const generateRandomInteger = (min, max) => {
-  return (min >= 0 && max >= 0) ? Math.round(Math.random() * (max - min + 1) + min) : false;
+  return (min >= 0 && max >= 0)
+    ? Math.round(Math.random() * (max - min + 1) + min)
+    : false;
 }
 
 const generateRandomFloat = (min, max, floatPrecision) => {
-  return (min >= 0 && max >= 0 && floatPrecision >= 0) ? (Math.random() * (max - min + 1) + min).toFixed(floatPrecision) : false;
+  return (min >= 0 && max >= 0 && floatPrecision >= 0)
+    ? (Math.random() * (max - min + 1) + min).toFixed(floatPrecision)
+    : false;
 }
 
 
