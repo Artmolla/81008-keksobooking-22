535d3bb551021db485c752d698a37e26dbff8f4a
Artem <artmolla@gmail.com>
2021-02-19 13:26:36 +0200

    Помощь друга #2

--- js/form.js
+++ js/form.js
@@ -4,11 +4,58 @@ const MIN_PRICES = {
   house: '5000',
   palace: '10000',
 }
+const MIN_TITLE_LENGTH = 30;
+const MAX_TITLE_LENGTH = 100;
+
+const listingTitle = document.querySelector('#title');
 const listingTypeSelect = document.querySelector('#type');
 const pricePerNightInput = document.querySelector('#price');
 const checkinTimeSelect = document.querySelector('#timein');
 const checkoutTimeSelect = document.querySelector('#timeout');
+const roomQuantityField = document.querySelector('#room_number');
+const guestQuantityField = document.querySelector('#capacity');
+const submitButton = document.querySelector('.ad-form__submit');
 export const adressField = document.querySelector('#address');
+
+const validateInput = (input) => {
+  if (input.validity.valueMissing === true) {
+    input.setCustomValidity('Пожалуйста, заполните это поле');
+  } else if (input.value.tooShort) {
+    input.setCustomValidity(`Описание слишком короткое минимальная длинна ${MIN_TITLE_LENGTH}`);
+  } else if (input.value.tooLong) {
+    input.setCustomValidity(`Описание слишком длинное максимальная длинна ${MAX_TITLE_LENGTH}`);
+  } else {
+    input.setCustomValidity('');
+  }
+
+  input.reportValidity();
+}
+
+const validatePrice = (input) => {
+  input.value > MIN_PRICES[listingTypeSelect.value]
+    ? input.setCustomValidity('')
+    : input.setCustomValidity(`Минимальная цена ${MIN_PRICES[listingTypeSelect.value]}`);
+}
+
+const setRoomGuestsDependence = (room, guestsList) => {
+  for (let guest of guestsList.children) {
+    if (room.value === '1' && guest.value === '1') {
+      guest.selected = true;
+      guest.disabled = false;
+    } else if (room.value === '2' && guest.value === '1' || room.value === '2' && guest.value === '2') {
+      guest.selected = true;
+      guest.disabled = false;
+    } else if (room.value === '3' && guest.value === '1' || room.value === '3' && guest.value === '2' || room.value === '3' && guest.value === '3') {
+      guest.selected = true;
+      guest.disabled = false;
+    } else if (room.value === '100' && guest.value === '0' || room.value === '0' && guest.value === '100') {
+      guest.selected = true;
+      guest.disabled = false;
+    } else {
+      guest.disabled = true;
+    }
+  }
+}
 
 const setTimeDependence = (checkIn, checkOut) => {
   for (let option of checkOut.children) {
@@ -17,6 +64,19 @@ const setTimeDependence = (checkIn, checkOut) => {
       : option.selected = false;
   }
 }
+
+listingTitle.addEventListener('input', () => {
+  const listingTitleLength = listingTitle.value.length;
+  if (listingTitleLength < MIN_TITLE_LENGTH) {
+    listingTitle.setCustomValidity(`Добавьте еще ${MIN_TITLE_LENGTH - listingTitleLength} символов`);
+  } else if (listingTitleLength > MAX_TITLE_LENGTH) {
+    listingTitle.setCustomValidity(`Слишком длинное описание, удалите ${listingTitleLength - MAX_TITLE_LENGTH} символов`);
+  } else {
+    listingTitle.setCustomValidity('');
+  }
+
+  listingTitle.reportValidity();
+})
 
 listingTypeSelect.addEventListener('change', () => {
   pricePerNightInput.min = MIN_PRICES[listingTypeSelect.value];
@@ -30,3 +90,16 @@ checkinTimeSelect.addEventListener('change', () => {
 checkoutTimeSelect.addEventListener('change', () => {
   setTimeDependence(checkoutTimeSelect, checkinTimeSelect);
 });
+
+guestQuantityField.addEventListener('change', () => {
+  setRoomGuestsDependence(guestQuantityField, roomQuantityField);
+})
+roomQuantityField.addEventListener('change', () => {
+  setRoomGuestsDependence(roomQuantityField, guestQuantityField);
+})
+
+submitButton.addEventListener('click', () => {
+  validateInput(listingTitle);
+  validateInput(pricePerNightInput);
+  validatePrice(pricePerNightInput);
+})
