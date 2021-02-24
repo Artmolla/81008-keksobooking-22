bb0c8c639ab4794c550dd51b0fb24001c058d85a
Artem <artmolla@gmail.com>
2021-02-24 17:27:45 +0200

    Поменял пути к иконкам

--- js/map.js
+++ js/map.js
@@ -38,14 +38,14 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [38, 95],
   iconAnchor: [22, 94],
   popupAnchor: [-3, -76],
 });
 
 export const pinIcon = L.icon({
-  iconUrl: '../img/pin.svg',
+  iconUrl: 'img/pin.svg',
   iconSize: [38, 95],
   iconAnchor: [22, 94],
   popupAnchor: [-3, -76],
