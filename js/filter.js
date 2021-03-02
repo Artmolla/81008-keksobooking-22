d8a21ade636e7b65190b4de64be6278aebc8ca64
Artem <artmolla@gmail.com>
2021-03-02 19:00:56 +0200

    Оптимизировал функции фильтрации по рекомендации наставника

--- js/filter.js
+++ js/filter.js
@@ -5,31 +5,31 @@ const roomSelect = mapFilters.querySelector('#housing-rooms');
 const guestSelect = mapFilters.querySelector('#housing-guests');
 const featuresField = mapFilters.querySelector('#housing-features');
 
-export const filterByPrice = (data) => {
+
+export const filterByPrice = (listing) => {
   switch (priceSelect.value) {
     case 'low':
-      return data.filter((listing) => listing.offer.price <= 1000);
+      return listing.offer.price <= 1000;
     case 'middle':
-      return data.filter((listing) => listing.offer.price >= 1000 && listing.offer.price <= 50000);
+      return listing.offer.price <= 50000;
     case 'high':
-      return data.filter((listing) => listing.offer.price >= 50000);
+      return listing.offer.price >= 50000;
     case 'any':
-      return data;
+      return listing;
   }
 };
 
-export const filterByFeatures = (data) => {
+export const filterByFeatures = (listing) => {
   const selectedFeatures = [].map.call(featuresField.querySelectorAll('input:checked'), (input) => input.value);
-
-  return data.filter(({offer: { features }}) => {
-    return selectedFeatures.every((feature) => features.includes(feature));
-  })
+  return selectedFeatures.every((feature) => listing.offer.features.includes(feature));
 };
 
 export const filterAll = (data) => {
-  return filterByFeatures(filterByPrice(data.filter((listing) => {
-    return (listing.offer.type === typeSelect.value|| typeSelect.value === 'any')
+  return data.filter((listing) => {
+    return (listing.offer.type === typeSelect.value || typeSelect.value === 'any')
       && (listing.offer.rooms === +roomSelect.value || roomSelect.value === 'any')
-      && (listing.offer.guests === +guestSelect.value || guestSelect.value === 'any');
-  })))
+      && (listing.offer.guests === +guestSelect.value || guestSelect.value === 'any')
+      && filterByPrice(listing)
+      && filterByFeatures(listing);
+  })
 }
