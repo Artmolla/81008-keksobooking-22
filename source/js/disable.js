cc145a939e212f35251bf33017715b34c43d91ad
Artem <artmolla@gmail.com>
2021-03-10 16:40:17 +0200

    Оптимизировал функцию для disable/enable елементов в файле disable.js

--- source/js/disable.js
+++ source/js/disable.js
@@ -2,21 +2,21 @@ import { mapFilters } from './filter.js';
 
 export const adForm = document.querySelector('.ad-form');
 
-const disableElements = (parent, selector) => {
-  parent.querySelectorAll(selector)
-    .forEach((element) => element.disabled = true);
-};
-
-export const enableElements = (parent, selector) => {
-  parent.querySelectorAll(selector)
-    .forEach((element) => element.disabled = false);
+export const setElementsAvailability = (parent, selector, available) => {
+  if (available === 'false') {
+    parent.querySelectorAll(selector)
+      .forEach((element) => element.disabled = true);
+  } else if (available === 'true') {
+    parent.querySelectorAll(selector)
+      .forEach((element) => element.disabled = false);
+  }
 };
 
 adForm.classList.add('ad-form--disabled');
 mapFilters.classList.add('map__filters--disabled');
-disableElements(adForm, 'input');
-disableElements(adForm, 'select');
-disableElements(adForm, 'button');
-disableElements(adForm, 'textarea');
-disableElements(mapFilters, 'input');
-disableElements(mapFilters, 'select');
+setElementsAvailability(adForm, 'input', 'false');
+setElementsAvailability(adForm, 'select', 'false');
+setElementsAvailability(adForm, 'button', 'false');
+setElementsAvailability(adForm, 'textarea', 'false');
+setElementsAvailability(mapFilters, 'input', 'false');
+setElementsAvailability(mapFilters, 'select', 'false');
